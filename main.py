a99473c9a5c673f6fc351759899b522b74e66cfc
root <69764760+root-7325@users.noreply.github.com>
2023-07-30 10:54:57 +0300

    Shitcode upgrade

--- main.py
+++ main.py
@@ -6,37 +6,40 @@ from random import randint
 
 class Main:
     def __init__(self):
-        self.api_id = None
-        self.api_hash = None
-        self.app = None
+        self.api_id_and_hash = None
+        self.app = []
+        self.chat_id = []
+        self.query = []
         self.wait_time = None
-        self.chat_id = None
-        self.query = None
 
     def read_config(self):
         with open('config.json', 'r', encoding='utf-8') as cfg:
             data = json.load(cfg)
-            self.api_id = data["config"]["api_id"]
-            self.api_hash = data["config"]["api_hash"]
+            self.api_id_and_hash = data["config"]["api_id_and_hash"].split("|")
+            self.api_hash = data["config"]["api_id_and_hash"].split("|")
             self.wait_time = data["config"]["wait_time"]
-            self.chat_id, self.query = [], []
-            for i in range(0, int(data["config"]["total_query"])):
-                self.query.append(data["search_query"][str(i + 1)])
-            for i in range(0, int(data["config"]["total_chat_id"])):
-                self.chat_id.append(data["chat_ids"][str(i + 1)])
-            self.app = Client("my_account", self.api_id, self.api_hash)
+            self.chat_id = data["config"]["chat_ids"].split("|")
+            self.query = data["config"]["queries"].split("|")
+        for id_x_hash in self.api_id_and_hash:
+            self.app.append(Client("my_account", id_x_hash.split(" ")[0], id_x_hash.split(" ")[1]))
 
     def start_app(self):
         self.read_config()
-        self.app.start()
+        for client in self.app:
+            client.start()
         self.start_sending()
 
     def start_sending(self):
         while True:
             try:
-                result = self.app.get_inline_bot_results("pic", self.query[randint(0, len(self.query) - 1)])
+                result = []
+                for client in self.app:
+                    result.append(client.get_inline_bot_results("pic", self.query[randint(0, len(self.query) - 1)]))
                 for id in self.chat_id:
-                    self.app.send_inline_bot_result(id, result.query_id, result.results[randint(0, len(result.results) - 1)].id)
+                    i = 0
+                    for client in self.app:
+                        client.send_inline_bot_result(id, result[i].query_id, result[i].results[randint(0, len(result[i].results) - 1)].id)
+                        i += 1
             except Exception:
                 print(f"dibilniki\n{traceback.format_exc()}")
             finally:
